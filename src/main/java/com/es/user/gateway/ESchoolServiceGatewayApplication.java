6cf8c32de240a6e44b526fc877a6bdb40d0c5ba2
WeisonWei <weixx3@126.com>
2020-03-23 13:58:30 +0800

    init

---
+++ src/main/java/com/es/user/gateway/ESchoolServiceGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.es.user.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ESchoolServiceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ESchoolServiceGatewayApplication.class, args);
+    }
+
+}
